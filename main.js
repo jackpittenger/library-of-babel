6fdfd8cac83bb62945ee72c8e1d5a90304ac0eab
Jack <31329139+realSaddy@users.noreply.github.com>
2021-11-23 19:21:07 -0600

    Experimenting

---
+++ main.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    const token = core.getInput('repo-token', { required: true });
+
+    const context = github.context;
+    if (context.payload.pull_request == null) {
+        core.setFailed('No pull request found.');
+        return;
+    }
+
+    const pull_request_number = context.payload.pull_request.number
+
+    const client = new github.GitHub(token);
+
+    const new_comment = client.issues.createComment({
+        ...context.repo,
+        issue_number: pull_request_number,
+        body: "hey!"
+    });
+
+    } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
