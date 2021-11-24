51c55436c318213383db340f8df6ea55b4336448
realSaddy <jrp30074@gmail.com>
2021-11-23 19:44:35 -0600

    Testing other token

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 async function run() {
   try {
-    const token = core.getInput('repo-token', { required: true });
+    const token = core.getInput("GITHUB_TOKEN", { required: true });
 
     const context = github.context;
     if (context.payload.pull_request == null) {
-        core.setFailed('No pull request found.');
-        return;
+      core.setFailed("No pull request found.");
+      return;
     }
 
-    const pull_request_number = context.payload.pull_request.number
+    const pull_request_number = context.payload.pull_request.number;
 
     const client = new github.GitHub(token);
 
     const new_comment = client.issues.createComment({
-        ...context.repo,
-        issue_number: pull_request_number,
-        body: "hey!"
+      ...context.repo,
+      issue_number: pull_request_number,
+      body: "hey!",
     });
-
-    } catch (error) {
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
 
 run();
+
